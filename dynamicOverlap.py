1b5b16f6f8847bf92dcdcb6563c4f1f04e52ced6
mugojames254 <mugojames254@gmail.com>
2025-05-22 11:03:14 +0900

    Updated the dynamic slicing code for improved processing speed

--- dynamicOverlap.py
+++ dynamicOverlap.py
@@ -6,26 +6,17 @@ import numpy as np
 def edgeDensity(image):
     image = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
 
-    if image.shape[-1] == 4:
-        image = image[:,:,:3]
-
     edges = cv2.Canny(image, threshold1=50, threshold2=150)
-    normalized_edges = edges / 255
+    normalized_edges = edges / 255.0
 
-    edge_density = np.sum(normalized_edges) / (image.shape[0] * image.shape[1])
-
+    edge_density = np.mean(normalized_edges)
+    
+    img_height, img_width = image.shape
+ 
     if edge_density > 0.1:
-        slice_height = image.shape[0] // 4
-        slice_width = image.shape[1] // 4
-        overlap_ratio = 0.4
+        return img_height // 4, img_width // 4, 0.4
     elif edge_density > 0.05:
-        slice_height = image.shape[0] // 3
-        slice_width = image.shape[1] // 3
-        overlap_ratio = 0.3
+        return img_height // 3, img_width // 3, 0.3
     else:
-        slice_height = image.shape[0] // 2
-        slice_width = image.shape[1] // 2
-        overlap_ratio = 0.2
-
-    return slice_height, slice_width, overlap_ratio
-
+        return img_height // 2, img_width // 2, 0.2
+    
